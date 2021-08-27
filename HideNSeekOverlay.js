cb575c97ebcca4fa9f163bb1079bb6e3c05f6549
JashSK <89659055+JashSK@users.noreply.github.com>
2021-08-27 14:54:58 -0500

    Added lightning and 2nd music track

--- HideNSeekOverlay.js
+++ HideNSeekOverlay.js
@@ -13,8 +13,10 @@
     'use strict';
     const coverScreen = document.createElement('div');
     const styleThingy = document.createElement('style');
-    const musicArray = ['https://seanysean.github.io/sk-hs-assets/Dark_80s_Horror_Music_-_Intruder_Royalty_Free_No_Copyright.mp3'];
+    const musicArray = ['https://seanysean.github.io/sk-hs-assets/Dark_80s_Horror_Music_-_Intruder_Royalty_Free_No_Copyright.mp3', 'https://seanysean.github.io/sk-hs-assets/REPULSIVE_-_Forgotten_COPYRIGHT_FREE_HORROR_MUSIC.mp3'];
+    const soundEffectsArray = ['https://seanysean.github.io/sk-hs-assets/lightning.mp3', 'https://seanysean.github.io/sk-hs-assets/lightning2.mp3'];
     let currentSong;
+    let lightningSound;
     let arrayOfTimeouts = [];
     let coverScreenOn = false;
     coverScreen.classList.add('seansimpossibletorememberclassname', 'seanshidethething');
@@ -40,7 +42,7 @@
         opacity: 0;
     }
     .jashwideview {
-        background: radial-gradient(RGBa(0, 245, 249, 0.4), RGBa(0, 245, 249, 0.4) 10%, #000 60%);
+        background: radial-gradient(RGBa(0, 245, 249, 0.2), RGBa(0, 245, 249, 0.2) 10%, #000 60%);
         background-position: 50% -10vh;
         width: 100%;
         border: none;
@@ -58,11 +60,14 @@ document.body.appendChild(coverScreen).appendChild(styleThingy);
                 endAllTimeouts();
                 currentSong.pause();
                 currentSong.load();
+                lightningSound.load();
+                lightningSound.pause();
             } else {
                 coverScreen.classList.remove('seanshidethething');
                 handleFlickerTimeOut();
                 if (!currentSong) {
                     currentSong = new Audio(musicArray[0]);
+                    lightningSound = new Audio(soundEffectsArray[0]);
                     currentSong.loop = true;
                 }
                 currentSong.play();
@@ -72,10 +77,14 @@ document.body.appendChild(coverScreen).appendChild(styleThingy);
     function handleFlickerTimeOut() {
         const timeoutInt = setTimeout(()=>{
             if (coverScreenOn) {
-                coverScreen.classList.add('jashwideview');
+                setTimeout(()=>{
+                    lightningSound = new Audio(soundEffectsArray[getRandomInt(0 , 1)]);
+                    lightningSound.play();
+                    coverScreen.classList.add('jashwideview');
+                }, 1500); // 1 seconds
                 setTimeout(()=>{
                     coverScreen.classList.remove('jashwideview');
-                },5000);
+                },5000); // 5 seconds
                 handleFlickerTimeOut();
             }
         }, 1000 * 20); // 20 seconds
@@ -92,6 +101,9 @@ document.body.appendChild(coverScreen).appendChild(styleThingy);
         coverScreen.style.borderLeft = `${borderSize + 1}px solid #000`;
         coverScreen.style.borderRight = `${borderSize + 1}px solid #000`;
         debugger;
+    }
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min)
     }
     addBorders();
 })();
